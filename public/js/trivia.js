205d82fece5fd71a3931209e619f4099ce57d482
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-07-25 18:10:21 +0300

    implement join, message,and  disconnect events

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -24,7 +24,83 @@ const mainHeadingTemplate = document.querySelector(
   // send event to server , describes the name of event , join to room
   socket.emit('join', { playerName, room }, error => {
     if (error) {
+        console.log('fjghkdfjghdkfj')
       alert(error);
       location.href = '/';
   }
   });
+
+  socket.on('message', ({ playerName, text, createdAt }) => {
+    console.log('ldjgkdfh.')
+    const chatMessages = document.querySelector('.chat__messages');
+    const messageTemplate = document.querySelector('#message-template').innerHTML;
+    const template = Handlebars.compile(messageTemplate);
+  
+    const html = template({
+      playerName,   
+      text,
+      createdAt: moment(createdAt).format('h:mm a'),
+    });
+    chatMessages.insertAdjacentHTML('afterBegin', html);
+  });
+  socket.on("room", ({ room, players }) => {
+   
+    // target the container where we'll attach the info to
+    const gameInfo = document.querySelector(".game-info");
+  
+    // target the Handlebars template we'll use to format the game info
+    const sidebarTemplate = document.querySelector(
+      "#game-info-template"
+    ).innerHTML;
+  
+    // Compile the template into HTML by calling Handlebars.compile(), which returns a function
+    const template = Handlebars.compile(sidebarTemplate);
+  
+    const html = template({
+      room,
+      players,
+    });
+  
+    // set gameInfo container's html content to the new html
+    gameInfo.innerHTML = html;
+  });
+
+  socket.on('room', ({ room, players }) => {
+    const gameInfo = document.querySelector('.game-info');
+    const sidebarTemplate = document.querySelector('#game-info-template')
+      .innerHTML;
+  
+    const template = Handlebars.compile(sidebarTemplate);
+  
+    const html = template({
+      room,
+      players,
+    });
+  
+    gameInfo.innerHTML = html;
+  });
+  
+  /*
+    CHAT SECTION
+  */
+  const chatForm = document.querySelector('.chat__form');
+  
+  chatForm.addEventListener('submit', event => {
+    event.preventDefault();
+  
+    const chatFormInput = chatForm.querySelector('.chat__message');
+    const chatFormButton = chatForm.querySelector('.chat__submit-btn');
+  
+    chatFormButton.setAttribute('disabled', 'disabled');
+  
+    const message = event.target.elements.message.value;
+  
+    socket.emit('sendMessage', message, error => {
+      chatFormButton.removeAttribute('disabled');
+      chatFormInput.value = '';
+      chatFormInput.focus();
+  
+      if (error) return alert(error);
+    });
+  });
+  
--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require("express");
 const path = require('path');
 const http= require('http');
 const socketio = require('socket.io');
-
+const formatMessage = require('./controller/formatMessage.js');
+const {
+  addPlayer,
+  getAllPlayers,
+  getPlayer,
+  removePlayer,
+} = require("./controller/players.js");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -12,10 +18,70 @@ app.use(express.static(publicPath));
 
 
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 io.on('connection', () => { // listen for new connections to Socket.IO
-    console.log('A new player just connected');
+   console.log('A new player just connected');
+    socket.on('join', ({ playerName, room }, next) => {
+        console.log('hi client')
+        const { error, newPlayer } = addPlayer({ id: socket.id, playerName, room });
+    
+        if (error) return next(error.message);
+        next(); // The callback can be called without data.
+    
+        socket.join(newPlayer.room);
+    
+        socket.emit('message', formatMessage('Admin', 'Welcome!'));
+        socket.broadcast
+        .to(newPlayer.room)
+        .emit(
+          'message',
+          formatMessage('Admin', `${newPlayer.playerName} has joined the game!`)
+        );
+        io.in(newPlayer.room).emit('room', {
+          room: newPlayer.room,
+          players: getAllPlayers(newPlayer.room),
+        });
+      
     });
+
+
+
+
+
+
+socket.on('sendMessage', (message, callback) => {
+    const { error, player } = getPlayer(socket.id);
+
+    if (error) return callback(error.message);
+
+    if (player) {
+      io.to(player.room).emit(
+        'message',
+        formatMessage(player.playerName, message)
+      );
+      callback();
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A player disconnected.');
+
+    const disconnectedPlayer = removePlayer(socket.id);
+
+    if (disconnectedPlayer) {
+      const { playerName, room } = disconnectedPlayer;
+      io.in(room).emit(
+        'message',
+        formatMessage('Admin', `${playerName} has left!`)
+      );
+
+      io.in(room).emit('room', {
+        room,
+        players: getAllPlayers(room),
+      });
+    }
+  });
+});
 server.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
-});
+}); 
