a23ad0b5e365dda4e03857a99e2d88b4290d6d19
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-07-25 14:26:08 +0300

    implement adding , removing player from room

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -2,13 +2,29 @@
 // uses it for transfering data between client and server
 const socket = io(); 
 
-// using https://handlebarsjs.com/guide/#installation for generateing player name 
+// using https://handlebarsjs.com/guide/#installation for generateing player name and room
 const SearchParams = new URLSearchParams(window.location.search);
 const playerName = SearchParams.get("playerName");
+const room = SearchParams.get('room');
 
 const mainHeadingTemplate = document.querySelector(
     "#main-heading-template"
   ).innerHTML;
 
+
+
   // Compile the template into HTML by calling compile method
   const welcomeHeadingHTML = Handlebars.compile(mainHeadingTemplate);
+  document.querySelector('main').insertAdjacentHTML(
+    'afterBegin',
+    welcomeHeadingHTML({
+      playerName,
+    })
+  );
+  // send event to server , describes the name of event , join to room
+  socket.emit('join', { playerName, room }, error => {
+    if (error) {
+      alert(error);
+      location.href = '/';
+  }
+  });
